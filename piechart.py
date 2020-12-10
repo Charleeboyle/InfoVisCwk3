02dfb6d94a71f913afec5d59514acd436cd21b5c
Dom Hackl <sc18d2h@leeds.ac.uk>
2020-12-10 16:16:56 +0000

    changed chart names

--- piechart.py
+++ piechart.py
@@ -11,10 +11,10 @@ def add_new_plot(plot, inputs, position, title):
 # plt.legend(title = "Degree Classification")
 label = '<40', '40-49', '50-59', '60-69', '>70'
 inputs = [
-    ([5,22,28,35,15], 1, 'Chart: A,  Degree Program: Geology'),
-    ([13,10,20,44,10], 2, 'Chart: B,  Degree Program: Geology'),
-    ([10, 25, 25, 30, 10], 3, 'Chart: C,  Degree Program: Geology'),
-    ([5,22,28,35,15], 4, 'Chart: D,  Degree Program: Geology'),
+    ([5,22,28,35,15], 1, 'Chart: A,  Degree Program: French'),
+    ([13,10,20,44,10], 2, 'Chart: B,  Degree Program: English'),
+    ([10, 25, 25, 30, 10], 3, 'Chart: C,  Degree Program: Maths'),
+    ([5,22,28,35,15], 4, 'Chart: D,  Degree Program: Biology'),
     ([13, 10, 20, 44, 10], 5, 'Chart: E,  Degree Program: Geology'),
     ([10, 25, 25, 30, 10], 6, 'Chart: F,  Degree Program: CS')
     ]
